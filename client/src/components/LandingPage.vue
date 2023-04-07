1dab1978d2a120d98c0c083104a393b265ec29fb
GuzlejM <martin.guzlej@gmmail.com>
2023-04-07 11:13:11 +0200

    edited LP added components

---
+++ client/src/components/LandingPage.vue
@@ -0,0 +1,15 @@
+<template lang="">
+  <div class="bg-gray-200 h-100 p-5">
+    <ContentWrapper />
+  </div>
+</template>
+<script>
+import ContentWrapper from './ContentWrapper.vue';
+export default {
+  name: "LandingPage",
+  components: { ContentWrapper },
+}
+</script>
+<style lang="">
+  
+</style>
